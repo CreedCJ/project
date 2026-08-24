72d176a76f17e37d74cc1eb8549d20fc27725c53
Your Name <your@email.com>
2026-08-24 10:30:01 -0700

    third push

---
+++ src/catalogue.js
@@ -0,0 +1,44 @@
+// ===============================
+// FRUITS DROPDOWN
+// ===============================
+
+const fruitsButton = document.getElementById("fruitsButton");
+const fruitOptions = document.getElementById("fruitOptions");
+const fruitsArrow = document.getElementById("fruitsArrow");
+
+
+fruitsButton.addEventListener("click", () => {
+
+    if (fruitOptions.classList.contains("grid-rows-[0fr]")) {
+
+        // OPEN
+        fruitOptions.classList.remove(
+            "grid-rows-[0fr]",
+            "opacity-0"
+        );
+
+        fruitOptions.classList.add(
+            "grid-rows-[1fr]",
+            "opacity-100"
+        );
+
+        fruitsArrow.classList.add("rotate-180");
+
+    } else {
+
+        // CLOSE
+        fruitOptions.classList.remove(
+            "grid-rows-[1fr]",
+            "opacity-100"
+        );
+
+        fruitOptions.classList.add(
+            "grid-rows-[0fr]",
+            "opacity-0"
+        );
+
+        fruitsArrow.classList.remove("rotate-180");
+
+    }
+
+});
